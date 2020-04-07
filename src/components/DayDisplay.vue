edd01918c5c7cc5fe6124d3f8fe1d3ec6c9e804d
zoulijie <zoulijie@u51.com>
2020-04-08 07:18:49 +0800

    feat: finish scheduleEvent Component

--- src/components/DayDisplay.vue
+++ src/components/DayDisplay.vue
@@ -1,19 +1,37 @@
 <template>
   <div class='day'>
-    {{date}}
+     <ScheduledEvent v-for='e in state.daysEvent' :key='e.id' :scheduledEvent='e' />
      <div class='hour' v-for='i in 24' :key='i' />
   </div>
 </template>
 
 <script>
 import moment from 'moment'
+import ScheduledEvent from './ScheduledEvent.vue';
+import { reactive } from 'vue';
 
 export default {
   props: {
     date: Object,
   },
-  setup () {
-
+  components: {
+    ScheduledEvent,
+  },
+  setup (props) {
+    const state = reactive({
+      daysEvent: [
+        {
+          id: '00',
+          name: 'Design Lecture',
+          startTime: moment(props.date).hour(5).minute(30),
+          endTime:  moment(props.date).hour(6).minute(30),
+          calendar: '01'
+        },
+      ]
+    })
+    return {
+      state
+    }
   }
 }
 </script>
