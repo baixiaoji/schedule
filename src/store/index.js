38d6ea46e8ddf1fcd734cb6b97f13803f42f3539
zoulijie <zoulijie@u51.com>
2020-04-08 16:36:49 +0800

    feat: finish mutation the store

--- src/store/index.js
+++ src/store/index.js
@@ -25,6 +25,48 @@ class PlannerStore extends Store {
       scheduledEvents: defaultData.events,
     };
   }
+
+  isTimeAvailable(id, startTime, endTime) {
+    let eventInDays = this.state.scheduledEvents.filter(e => {
+      return e.id === id && moment(e.startTime).isSame(startTime, 'day');
+    });
+
+    let range = moment.range(startTime, endTime);
+
+    for (let i = 0; i < eventInDays.length; i++) {
+      let eRange = moment.range(eventInDays[i].startTime, eventInDays[i].endTime);
+
+      if (eRange.overlaps(range, {adjacent: false})) {
+        return {
+          overlap: eventInDays[i],
+          available: false,
+        }
+      }
+    }
+
+    return {
+      available: true,
+    }
+
+  }
+
+  editEvent(newEvent) {
+    let e = this.state.scheduledEvents.find(e => e.id === newEvent.id);
+
+    if (!e) {
+      return false;
+    }
+
+    if (this.isTimeAvailable(newEvent.id, newEvent.startTime, newEvent.endTime).available) {
+      e.name = newEvent.name;
+      e.calendar = newEvent.calendar;
+      e.startTime = newEvent.startTime;
+      e.endTime = newEvent.endTime;
+      return true;
+    }
+
+    return false;
+  }
 }
 
 export const store = new PlannerStore();
